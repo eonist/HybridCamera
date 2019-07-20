bf3a5a7235052a7618cc79aad32808dbf078cb36
Eonist <30n1st@gmail.com>
2019-07-20 19:44:38 +0200

    Update carthage build

--- Carthage/Checkouts/With/WithExample/AppDelegate.swift
+++ Carthage/Checkouts/With/WithExample/AppDelegate.swift
@@ -1,9 +1,15 @@
 import UIKit
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-   var window: UIWindow?
+   lazy var window: UIWindow? = {
+      let win = UIWindow(frame: UIScreen.main.bounds)
+      let vc = ViewController()
+      win.rootViewController = vc
+      win.makeKeyAndVisible()/*Important since we have no Main storyboard anymore*/
+      return win
+   }()
    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      _ = window
       return true
    }
 }
