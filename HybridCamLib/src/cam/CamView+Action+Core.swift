f9f21bbd3fa1458882cd621d883d729bedc90d3a
blurtime <moritzfreyberg@aol.com>
2019-07-22 15:35:42 +0200

    Update CamView+Action+Core.swift
    one-liner instead of 2 `if`s

--- HybridCamLib/src/cam/CamView+Action+Core.swift
+++ HybridCamLib/src/cam/CamView+Action+Core.swift
@@ -50,11 +50,7 @@ extension CamView {
             onVideoCaptureComplete(nil, error)
          }
       }
-      if connection.isVideoMirroringSupported {
-         if device.position == .front {
-            connection.isVideoMirrored = true
-         }
-      }
+      connection.isVideoMirrored = (connection.isVideoMirroringSupported && device.position == .front)
       guard let outputURL: URL = CamUtil.tempURL() else { onVideoCaptureComplete(nil, CaptureError.noTempFolderAccess); return }
       videoOutput.startRecording(to: outputURL, recordingDelegate: self)
    }
