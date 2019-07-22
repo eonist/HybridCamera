fc13df39d6e812cdd570f3a4048dc8fca6b413c1
blurtime <moritzfreyberg@aol.com>
2019-07-22 20:21:37 +0200

    Update CamView+Action+Core.swift

--- HybridCamLib/src/cam/CamView+Action+Core.swift
+++ HybridCamLib/src/cam/CamView+Action+Core.swift
@@ -50,7 +50,7 @@ extension CamView {
             onVideoCaptureComplete(nil, error)
          }
       }
-      connection.isVideoMirrored = (connection.isVideoMirroringSupported && device.position == .front && mirrorFrontCamera)
+      connection.isVideoMirrored = connection.isVideoMirroringSupported && device.position == .front
       guard let outputURL: URL = CamUtil.tempURL() else { onVideoCaptureComplete(nil, CaptureError.noTempFolderAccess); return }
       videoOutput.startRecording(to: outputURL, recordingDelegate: self)
    }
