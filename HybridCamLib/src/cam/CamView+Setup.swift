67f0cfa429b93a29469992bd787a7d7684118f05
blurtime <moritzfreyberg@aol.com>
2019-07-22 08:06:07 +0200

    Change standard camera to front

--- HybridCamLib/src/cam/CamView+Setup.swift
+++ HybridCamLib/src/cam/CamView+Setup.swift
@@ -10,7 +10,7 @@ extension CamView {
     */
    @objc open func setupDevice() {
       do {
-         try setupCaptureDeviceInput(cameraType: .back)
+         try setupCaptureDeviceInput(cameraType: .front) //used to be .back
          try setupVideoCamera()
          try setupMicrophone()
          try setupPhotoCamera()
