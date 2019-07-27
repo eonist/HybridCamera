e9fe946dbd411aa21f2d5a546b165f45c600e41a
blurtime <moritzfreyberg@aol.com>
2019-07-27 14:55:11 +0200

    Update ProcessView+Event.swift
    mem leak fix improvement

--- HybridCamLib/src/process/ProcessView+Event.swift
+++ HybridCamLib/src/process/ProcessView+Event.swift
@@ -10,14 +10,16 @@ extension ProcessView {
     * AttachCallBacks
     */
    @objc open func addEventListeners() {
-      exitButton.onClick = { self.onExit() } // Warning: ⚠️️ this needs to be called like it is, not simply assigned
-      shareButton.onClick = {/*launch share AlertDialog*/
-         if let imageURL = self.imageView.url {
-            self.onShare( imageURL )
-         } else if let videoURL = (self.videoPlayerView.avPlayer.currentItem?.asset as? AVURLAsset)?.url {
-            self.onShare(videoURL)
+      exitButton.onClick = { [weak self] in
+         self?.onExit()
+      } // Warning: ⚠️️ this needs to be called like it is, not simply assigned
+      shareButton.onClick = { [weak self] in /*launch share AlertDialog*/
+         if let imageURL = self?.imageView.url {
+            self?.onShare( imageURL )
+         } else if let videoURL = (self?.videoPlayerView.avPlayer?.currentItem?.asset as? AVURLAsset)?.url {
+            self?.onShare(videoURL)
          } else {
-            self.onShare(nil)
+            self?.onShare(nil)
          }
       }
    }
