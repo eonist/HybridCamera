69014ad7adaa345d0617f6d576e08ffe818562ab
André J <30n1st@gmail.com>
2021-08-20 11:48:01 +0200

    Bump to iOS 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "HybridCamera",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "HybridCamera",
